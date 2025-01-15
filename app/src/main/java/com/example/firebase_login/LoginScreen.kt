f7db4bbbcea6b8a664f280251326972cb200525f
sowro <sowrovnil59@gmail.com>
2025-01-16 00:50:19 +0600

    firebase_login_sig data ui complite

--- app/src/main/java/com/example/firebase_login/LoginScreen.kt
+++ app/src/main/java/com/example/firebase_login/LoginScreen.kt
@@ -1,22 +1,43 @@
 package com.example.firebase_login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.firebase_login.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
+    lateinit var binding: ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityLoginScreenBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_screen)
+        setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.buttonSignup.setOnClickListener {
+
+
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/firebase_login/SignUpActivity.kt
+++ app/src/main/java/com/example/firebase_login/SignUpActivity.kt
@@ -1,20 +1,36 @@
 package com.example.firebase_login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.firebase_login.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivitySignUpBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+binding.SignUp.setOnClickListener {
+
+    val intent = Intent(this, LoginScreen::class.java)
+    startActivity(intent)
+    finish()
+
+
+}
+        
     }
 }
