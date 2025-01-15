e291d0f69d259f29017a82a3d401f0390ac4b40e
sowro <sowrovnil59@gmail.com>
2025-01-15 23:27:31 +0600

    firebase_login_sig data

---
+++ app/src/main/java/com/example/firebase_login/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.firebase_login
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firebase_login.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+        }, 3000)
+
+
+        val welcomeText = "Welcome"
+        val spannableString = SpannableString(welcomeText)
+        spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#FF0000")), 0, 5, 0)
+        spannableString.setSpan(
+            ForegroundColorSpan(Color.parseColor("#000000")),
+            5,
+            welcomeText.length,
+            0
+        )
+        binding.welcomeText.text = spannableString
+
+
+    }
+
+
+}
